a0ebe08f8e3d09f661d4f9bec5ac0c8062373c7f
Antony Chacko <antony101tony@gmail.com>
2024-11-18 17:15:04 +0800

    initial commit - created app outline

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Fortnight work hour calculator</h1>
+            <button>create new employer</button>
+            <button>log completed shift</button>
+            <div>
+                <h2>Hours worked</h2>
+                <h3>fortnight starting last week</h3>
+                <div>23</div>
+                <h3>fortnight ending next week</h3>
+                <div>21</div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
